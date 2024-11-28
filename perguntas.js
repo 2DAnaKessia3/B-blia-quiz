8554516f47bb294ed3791c75d1eb0f2611b9fc8d
2DAnaKessia3 <slxpie347@gmail.com>
2024-11-28 14:21:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Bíblia',
+    'Além de Caim e Abel, qual é o nome do outro filho de Adão e Eva, que a Bíblia cita? ',
+    'Sete'
+)
+
+criaCartao(
+    'Bíblia',
+    'Qual foi o profeta, que andou nu e descalço durante três anos seguidos, por sinal de prodígio sobre o Egito e a Etiópia? ',
+    'Isaías'
+)
+
+criaCartao(
+    'Bíblia',
+    'Qual era o destino de Jonas, após tentar fugir da presença de Deus, e negar a missão de alertar os moradores de Nínive sobre o jugo de Deus sobre eles? ',
+    'Társis'
+)
+
+criaCartao(
+    'Bíblia',
+    'Qual foi o profeta que, quando duvidou da palavra de Deus, através de um anjo acerca de que sua mulher iria conceber um filho já na velhice, ficou mudo? ',
+    'Zacarias'
+)
